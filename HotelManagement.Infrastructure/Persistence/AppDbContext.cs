13cc9b28f81197ad267560922b8b38caa67ac6c9
Catalan-1ap <catalan.1ap.main@gmail.com>
2022-02-17 02:10:50 +0300

    Local database changes
    Local MSSQL is now in file (Infrastructure project)
    
    Migrations folder moved into Persistence

--- HotelManagement.Infrastructure/Persistence/AppDbContext.cs
+++ HotelManagement.Infrastructure/Persistence/AppDbContext.cs
@@ -1,3 +1,4 @@
+using System;
 using HotelManagement.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using SmartEnum.EFCore;
@@ -7,19 +8,20 @@ namespace HotelManagement.Infrastructure.Persistence
 {
 	public class AppDbContext : DbContext
 	{
-		internal static string ConnectionsString = @"Server=(localdb)\mssqllocaldb;Database=HotelManagement;Trusted_Connection=True;"; 
-		
+		internal static readonly string ConnectionsString = 
+			@$"Server=(localdb)\mssqllocaldb;AttachDbFilename={Environment.CurrentDirectory}\Persistence\Database\HotelManagement.mdf;Trusted_Connection=True;";
+
 		public DbSet<Room> Rooms { get; set; } = null!;
 		public DbSet<Person> Persons { get; set; } = null!;
 		public DbSet<Client> Clients { get; set; } = null!;
 		public DbSet<Cleaner> Cleaners { get; set; } = null!;
 		public DbSet<Floor> Floors { get; set; } = null!;
-		
+
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			base.OnConfiguring(optionsBuilder);
-			
+
 			optionsBuilder.UseSqlServer(ConnectionsString);
 		}
 
@@ -27,7 +29,7 @@ namespace HotelManagement.Infrastructure.Persistence
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			base.OnModelCreating(modelBuilder);
-			
+
 			modelBuilder.ConfigureSmartEnum();
 
 			modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
